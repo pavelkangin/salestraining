d9a932ae246bd2107c435822734a9ee232110553
kapv <kapv@bk.ru>
2023-11-01 20:22:14 +0300

    initial commit

---
+++ src/main/java/com/st/app/SimpleController.java
@@ -0,0 +1,58 @@
+package com.st.app;
+
+import com.st.app.dao.GreetingDAO;
+import com.st.app.model.Greeting;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@RestController
+public class SimpleController {
+
+    @GetMapping("/hello")
+    public Greeting helloWorld(){
+        //
+        return new Greeting("Ivan","Hello!");
+    }
+
+    @PostMapping("/auth")
+    public Greeting authenticate(){
+
+        return new Greeting("Ivan","Hello!");
+    }
+
+    @GetMapping
+    public List<Greeting> getLessons(String searchText){
+        /*
+
+        GreetingValidate.check(searchText)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+         */
+        return GreetingDAO.getGreetings();
+    }
+}
---
+++ src/main/java/com/st/app/dao/GreetingDAO.java
@@ -0,0 +1,14 @@
+package com.st.app.dao;
+
+import com.st.app.model.Greeting;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class GreetingDAO {
+
+    public static List<Greeting> getGreetings(){
+
+        return new ArrayList<>();
+    }
+}
